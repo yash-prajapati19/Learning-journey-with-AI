882432acf75e4ce19f4d6c6bb6da7cbb0b8e0dc4
Yash Prajapati <yp54139@gmail.com>
2023-12-09 21:35:02 +0530

    navbar

---
+++ src/components/UserAccountNav.tsx
@@ -0,0 +1,51 @@
+"use client";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import React from "react";
+import { User } from "next-auth";
+import { signOut } from "next-auth/react";
+import { LogOut } from "lucide-react";
+import UserAvatar from "./UserAvatar";
+
+type Props = {
+  user: User;
+};
+
+const UserAccountNav = ({ user }: Props) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar user={user} />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <div className="flex items-center justify-start gap-2 p-2 bg-white">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user?.name && <p className="font-medium">{user.name}</p>}
+            {user?.email && (
+              <p className="w-[200px] truncate text-sm text-secondary-foreground">
+                {user.email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onSelect={() => {
+            signOut();
+          }}
+          className="text-red-600 cursor-pointer p-2"
+        >
+          Sign out
+          <LogOut className="w-4 h-4 ml-2" />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserAccountNav;
